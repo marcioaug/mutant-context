fe9a93e9565be367d12ee77ef097a4eab8452334
Marcio Augusto Guiamarães <marcioaugustosg@gmail.com>
2018-03-05 07:39:50 -0300

    Primeira execução do TCE

---
+++ tests/Main.java
@@ -0,0 +1,19 @@
+import Class1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Class1 class1 = new Class1();
+    }
+
+    public int initialize() {
+        Test test = new Test();
+        return test.method();
+    }
+
+    private class Test {
+        public int method() {
+            return 1;
+        }
+    }
+}
