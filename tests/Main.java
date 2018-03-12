d71ca5a8ef006933a7f7b4651d71b6836b612497
Marcio Augusto Guiamarães <marcioaugustosg@gmail.com>
2018-03-11 21:32:32 -0300

    change subjects-list

--- tests/Main.java
+++ tests/Main.java
@@ -2,7 +2,7 @@ import Class1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worl.ld!");
         Class1 class1 = new Class1();
     }
 
